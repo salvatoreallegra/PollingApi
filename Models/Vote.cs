42ccdeb292575b50e23db42130afb56d227e152c
Salvatore Allegra <salallegra@gmail.com>
2025-08-16 17:08:28 -0700

    Create vote model

---
+++ Models/Vote.cs
@@ -0,0 +1,11 @@
+﻿namespace PollingApi.Models
+{
+    public class Vote
+    {
+        public int Id { get; set; }
+        public int PollId { get; set; }
+        public int OptionId { get; set; }
+        public required string UserId { get; set; }
+        public DateTime VotedAt { get; set; }
+    }
+}
